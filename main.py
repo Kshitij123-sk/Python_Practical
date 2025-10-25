052964c4961b68244c812a8206970ab688a82b0c
Kshitij123-sk <kshitijovhal342@gmail.com>
2025-10-25 21:01:32 +0530

    intial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello kshitij");
+num1=int(input("enter the first no :"));
+num2=int(input("enter the second no :"));
+
+
+print("result : ");
+print("Addition : \n",num1+num2);
+print("substraction : \n",num1-num2);
+print("division : \n",num1/num2);
+print("multiplication : \n",num1*num2);
+
