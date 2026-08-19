18f9daae48f6fc3527c0c8a3b9c8ff27a567bf66
Kshitij123-sk <kshitijovhal342@gmail.com>
2026-08-19 18:11:55 +0530

    operation on tuple multiplication

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,5 +1,2 @@
-student_details1 = (1001, "John Doe", "Computer Science", 3.8)
-student_details2 = (1002, "Jane Smith", "Mathematics", 3.9)
-
-student_details = student_details1 + student_details2
-print(student_details)
+t1 = ("class 5" , 5000)
+print(t1 * 3)
