d389189a00a3368893b794800920b648278a1fba
Kshitij123-sk <kshitijovhal342@gmail.com>
2026-08-19 18:17:28 +0530

    tuple operation on boolean

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,2 +1,5 @@
-t1 = ("class 5" , 5000)
-print(t1 * 3)
+student_details = (1001, "john")
+student_details2 = (78.2 , 91.5, 88.0)
+
+print(78.2 in student_details2)  # Output: True
+print(87.5 in student_details2)  # Output: False
