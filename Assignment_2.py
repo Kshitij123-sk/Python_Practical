577cb00e2da52027a65ed4d9229e1cf7a9f7134c
Kshitij123-sk <kshitijovhal342@gmail.com>
2026-08-19 18:03:18 +0530

    assignment_2

---
+++ Assignment_2.py
@@ -0,0 +1,5 @@
+student_details1 = (1001, "John Doe", "Computer Science", 3.8)
+student_details2 = (1002, "Jane Smith", "Mathematics", 3.9)
+
+student_details = student_details1 + student_details2
+print(student_details)
